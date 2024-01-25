fe125abf0d32a694a874be519195cb60bb88ef62
@abhishek.mohan <abhishek.mohan@surboard.se>
2024-01-25 15:32:16 +0530

    with signal computed and untracked:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,signal,computed,OnInit,untracked } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'signalsapp';
+
+  counter = signal(0)
+  //computed
+  computedCounter = computed(() => {
+        return this.counter()*100 //computed -> works by taking in one or more source signals
+        // return untracked(this.counter)*100 //untracked -> for not being dependent
+  })
+
+  list = signal(['Hello','World'])
+
+
+ngOnInit(): void {
+
+}
+
+
+  increment(){
+
+    this.counter.set(this.counter() + 1)  //set method
+    // this.counter.update(counter => counter + 1) //update method
+    console.log(this.counter())
+  }
+
+
 }
